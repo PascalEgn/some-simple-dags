31c79887b744619913f52cb0df63c66f7d1a38ae
PascalEgn <pascal.egner@googlemail.com>
2025-11-27 17:15:58 +0100

    testing

--- dags/test_logging_dag.py
+++ dags/test_logging_dag.py
@@ -3,22 +3,21 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 import logging
 
-def log_message(task_name, **kwargs):
+
+def log_message(task_name):
     logger = logging.getLogger("airflow.task")
 
     now = datetime.utcnow().isoformat()
     logger.info(f"[{task_name}] Log generated at: {now}")
     logger.warning(f"[{task_name}] Warning event at: {now}")
     logger.error(f"[{task_name}] Error event at: {now}")
-
     for handler in logger.handlers:
-        print("Handler found:", handler)
-
+        print(handler)
 
 with DAG(
     dag_id="test_logging_dag",
-    description="Simple DAG for testing logs sent to OpenSearch",
-    schedule_interval=None,
+    description="Simple DAG for testing logs sent to Elasticsearch",
+    schedule="*/5 * * * *",
     start_date=datetime(2025, 1, 1),
     catchup=False,
     tags=["test", "logging"],
@@ -28,21 +27,18 @@ with DAG(
         task_id="log_task_a",
         python_callable=log_message,
         op_kwargs={"task_name": "Task A"},
-        provide_context=True,
     )
 
     task_b = PythonOperator(
         task_id="log_task_b",
         python_callable=log_message,
         op_kwargs={"task_name": "Task B"},
-        provide_context=True,
     )
 
     task_c = PythonOperator(
         task_id="log_task_c",
         python_callable=log_message,
         op_kwargs={"task_name": "Task C"},
-        provide_context=True,
     )
 
     task_a >> task_b >> task_c
