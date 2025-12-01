e324efd1ca74bfe28a0148e90184ea606f460682
PascalEgn <pascal.egner@googlemail.com>
2025-12-01 15:53:54 +0100

    fix

--- dags/test_logging_dag.py
+++ dags/test_logging_dag.py
@@ -11,7 +11,6 @@ def log_message(task_name):
     logger.info(f"[{task_name}] Log generated at: {now}")
     logger.warning(f"[{task_name}] Warning event at: {now}")
     logger.error(f"[{task_name}] Error event at: {now}")
-    self.log.info("This is a test log message")
     print(logger)
     print(logger.handlers)
 
